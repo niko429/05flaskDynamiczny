f240dfd6612080d63ba8d5f510aa182745587dab
niko429 <tomjeryy16@gmail.com>
2026-09-17 13:30:38 +0300

    Zrobiono drugie zadanie

--- appZadania.py
+++ appZadania.py
@@ -8,5 +8,24 @@ app = Flask(__name__)
 def czesc(imie):
     return f"Cześć {imie}"
 
+# Zadanie 2
+@app.route("/dodaj/<int:a>/<int:b>")
+def dodaj(a, b):
+    return f"{a} + {b} = {a + b}"
+
+@app.route("/odejmij/<int:a>/<int:b>")
+def odejmij(a, b):
+    return f"{a} - {b} = {a - b}"
+
+@app.route("/pomnoz/<int:a>/<int:b>")
+def pomnoz(a, b):
+    return f"{a} * {b} = {a * b}"
+
+@app.route("/podziel/<int:a>/<int:b>")
+def podziel(a, b):
+    if b == 0:
+        return "Nie dzielimy przez zero", 400
+    return f"{a} / {b} = {a /b}"
+
 if __name__ == "__main__":
     app.run(debug=True)
