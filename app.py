aa865d8884d62ad246ef19f452e9355aa45f3149
Niko985642 <tomjeryy16@gmail.com>
2026-09-24 21:03:42 +0200

    Zrobiono drugą część zadania oraz poprawiono pierwszą część w takim sensie że brakowało przecinka w pierwszej części zadania w returnie

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/czesc/<imie>")
 def imie(imie):
-    return f"Cześć {imie}"
+    return f"Cześć, {imie}"
+
+@app.route("czesc/<imie>/<int:wiek>")
+def imieWiek(imie,wiek):
+    return f"Cześć, {imie}, masz {wiek} lat"
 
 if __name__ == "__main__":
     app.run(debug=True)
